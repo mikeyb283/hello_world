c7f71dc1235478f21814259fc17c92fc06589fc0
Michael Balling <mballing@students.mchenry.edu>
2018-01-21 18:44:41 -0600

    Initial commit

---
+++ src/hello_world.java
@@ -0,0 +1,7 @@
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String aString = "Mike Balling";
+        System.out.println("My name is " + aString);
+    }
+}
